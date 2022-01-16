947a12856cae3970d1793d0d27b319aa2445414e
FigmentBoy <17240592+FigmentBoy@users.noreply.github.com>
2022-01-15 23:51:16 -0600

    Hopefully make windows work

--- Source/Cacao/gen/InterfaceGen.cpp
+++ Source/Cacao/gen/InterfaceGen.cpp
@@ -56,21 +56,21 @@ struct ${class_name} : {class_name}, InterfaceBase {{
     // requires: class_name, function_name, const, index, raw_args, arg_types, parameters, convention
     char const* declare_meta_member = R"CAC(
     dupable ret{index} {function_name}({raw_args}) {const}{{
-        return lilac::meta::Function<ret{index}(decltype(this){arg_types})>, lilac::meta::x86::{convention}>{{address{index}}(this{parameters});
+        return lilac::meta::Function<ret{index}(decltype(this){arg_types})>, lilac::meta::x86::{convention}<address{index}>(this{parameters});
     }}
 )CAC";
 
     // requires: class_name, function_name, const, index, arg_types, raw_parameters, raw_args, convention
     char const* declare_meta_static = R"CAC(
     static dupable ret{index} {function_name}({raw_args}) {const}{{
-        return lilac::meta::Function<ret{index}(decltype(this){arg_types})>, lilac::meta::x86::{convention}>{{address{index}}({raw_parameters});
+        return lilac::meta::Function<ret{index}(decltype(this){arg_types})>, lilac::meta::x86::{convention}<address{index}>({raw_parameters});
     }}
 )CAC";
 
     // requires: function_name, raw_args, arg_types, index, parameters, convention
     char const* declare_meta_structor = R"CAC(
     dupable void {function_name}({raw_args}) {{
-        return lilac::meta::Function<void(decltype(this){arg_types})>, lilac::meta::x86::{convention}>{{address{index}}(this{parameters});
+        return lilac::meta::Function<void(decltype(this){arg_types})>, lilac::meta::x86::{convention}<address{index}>(this{parameters});
     }}
 )CAC";
 
@@ -106,7 +106,6 @@ int main(int argc, char** argv) {
         string unqualifiedName = CacShare::toUnqualified(name);
 
         output += fmt::format(format_strings::interface_start, fmt::arg("class_name", unqualifiedName));
-
         for (auto& f : c.functions) {
             if (f.binds[CacShare::platform].size() == 0)
                 continue; // Function not supported for this platform, skip it
